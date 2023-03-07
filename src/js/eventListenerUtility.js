594e929279252ea709ea96e3c9369dafaa66330a
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2023-03-07 17:27:09 +0100

    m1&2_start

--- src/js/eventListenerUtility.js
+++ src/js/eventListenerUtility.js
@@ -1,11 +1,12 @@
 import switchToTab from "./tabUtility.js";
+import {switchToView} from "./viewUtility.js";
 
 export default function coupleEventListeners() {
     // Register clickthrough on all switch tab elements
     const stElements = document.querySelectorAll('.switch-tab');
 
     stElements.forEach(elem => {
-        elem.addEventListener("click",() => {
+        elem.addEventListener("click", () => {
             const tabName = elem.getAttribute('data-tab-name');
             switchToTab(tabName)
 
@@ -19,20 +20,15 @@ export default function coupleEventListeners() {
     const addMissionView = document.getElementById('add-mission');
     const editMissionView = document.getElementById('edit-mission');
 
-    btnAddMissionView.addEventListener("click",() => {
-        // Hide edit mission view
-        editMissionView.classList.add('d-none');
+    function showView(view) {
+        view.classList.remove('d-none');
+    }
 
-        // Show add mission view
-        addMissionView.classList.remove('d-none');
+    btnAddMissionView.addEventListener("click", () => {
+        switchToView('add-mission')
     })
 
-
-    btnEditMissionView.addEventListener("click",() => {
-        // Hide add mission view
-        addMissionView.classList.add('d-none');
-
-        // Show edit mission view
-        editMissionView.classList.remove('d-none');
+    btnEditMissionView.addEventListener("click", () => {
+       switchToView('edit-mission')
     })
 }
